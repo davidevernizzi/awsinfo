24bbda99a7b59a792867346b5983cc4b8443699d
Davide Vernizzi <davide.vernizzi@gmail.com>
2013-11-22 00:22:25 +0100

    parsed secrets.yml

--- awsinfo.rb
+++ awsinfo.rb
@@ -15,11 +15,22 @@ require 'rubygems'
 require 'yaml'
 require 'aws-sdk'
 
-config_file = File.join(File.dirname(__FILE__),
-                        "config.yml")
+config_file = File.join(File.dirname(__FILE__), "config.yml")
+secrets_file = File.join(File.dirname(__FILE__), "secrets.yml")
+
 unless File.exist?(config_file)
   puts <<END
-To run the samples, put your credentials in config.yml as follows:
+To run the samples, put your configuration in config.yml as follows:
+
+region: xxx
+
+END
+  exit 1
+end
+
+unless File.exist?(secrets_file)
+  puts <<END
+To run the samples, put your credentials in secrets.yml as follows:
 
 access_key_id: YOUR_ACCESS_KEY_ID
 secret_access_key: YOUR_SECRET_ACCESS_KEY
@@ -29,10 +40,21 @@ END
 end
 
 config = YAML.load(File.read(config_file))
+secrets = YAML.load(File.read(secrets_file))
 
 unless config.kind_of?(Hash)
   puts <<END
 config.yml is formatted incorrectly.  Please use the following format:
+
+region: xxx
+
+END
+  exit 1
+end
+
+unless secrets.kind_of?(Hash)
+  puts <<END
+secrets.yml is formatted incorrectly.  Please use the following format:
 
 access_key_id: YOUR_ACCESS_KEY_ID
 secret_access_key: YOUR_SECRET_ACCESS_KEY
@@ -41,7 +63,7 @@ END
   exit 1
 end
 
-AWS.config(config)
+AWS.config(config.merge(secrets))
 
 ec2 = AWS.ec2
 
