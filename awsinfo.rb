db70cbdd692fa166c0740a211f0fd1d7a6feea9a
Davide Vernizzi <davide.vernizzi@gmail.com>
2013-11-23 00:23:14 +0100

    refactored into a class

--- awsinfo.rb
+++ awsinfo.rb
@@ -1,99 +1,75 @@
-# Copyright 2011-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You
-# may not use this file except in compliance with the License. A copy of
-# the License is located at
-#
-#     http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file. This file is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-# ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
-
 require 'rubygems'
 require 'yaml'
 require 'aws-sdk'
 
-config_file = File.join(File.dirname(__FILE__), "config.yml")
-secrets_file = File.join(File.dirname(__FILE__), "secrets.yml")
+class AwsInfo
 
-unless File.exist?(config_file)
-  puts <<END
-To run the samples, put your configuration in config.yml as follows:
+    def parse_config_files config_file, secrets_file
+        config_file = File.join(File.dirname(__FILE__), "config.yml")
+        secrets_file = File.join(File.dirname(__FILE__), "secrets.yml")
 
-region: xxx
+        unless File.exist?(config_file)
+            puts "Fill config.yml"
+            exit 1
+        end
 
-END
-  exit 1
-end
+        unless File.exist?(secrets_file)
+            puts "Fill secrets.yml"
+            exit 1
+        end
 
-unless File.exist?(secrets_file)
-  puts <<END
-To run the samples, put your credentials in secrets.yml as follows:
+        config = YAML.load(File.read(config_file))
+        secrets = YAML.load(File.read(secrets_file))
 
-access_key_id: YOUR_ACCESS_KEY_ID
-secret_access_key: YOUR_SECRET_ACCESS_KEY
+        unless config.kind_of?(Hash)
+            puts "config.yml is formatted incorrectly.  Please use the following format:"
+            puts "region: xxx"
+            exit 1
+        end
 
-END
-  exit 1
-end
+        unless secrets.kind_of?(Hash)
+            puts "secrets.yml is formatted incorrectly.  Please use the following format:"
+            puts " access_key_id: YOUR_ACCESS_KEY_ID"
+            puts "secret_access_key: YOUR_SECRET_ACCESS_KEY"
+            exit 1
+        end
 
-config = YAML.load(File.read(config_file))
-secrets = YAML.load(File.read(secrets_file))
+        AWS.config(config.merge(secrets))
+    end
 
-unless config.kind_of?(Hash)
-  puts <<END
-config.yml is formatted incorrectly.  Please use the following format:
+    def get_by_autoscaling_group
+        ec2 = AWS.ec2
 
-region: xxx
-
-END
-  exit 1
-end
-
-unless secrets.kind_of?(Hash)
-  puts <<END
-secrets.yml is formatted incorrectly.  Please use the following format:
-
-access_key_id: YOUR_ACCESS_KEY_ID
-secret_access_key: YOUR_SECRET_ACCESS_KEY
-
-END
-  exit 1
-end
-
-AWS.config(config.merge(secrets))
-
-ec2 = AWS.ec2
-
-as_sg = AWS::AutoScaling.new
-sg = as_sg.groups
-sg.each do |x|
-   puts x.name
-   x.auto_scaling_instances.each do |instance|
-      puts ec2.instances[instance.instance_id].dns_name
-   end
-end
-
-elb = AWS::ELB.new
-
-elb.load_balancers.each do |e|
-
-    ic = AWS::ELB::InstanceCollection.new e
-
-    puts ic.load_balancer.name
-    ic.health.each do |instance_health|
-        if instance_health[:state] == 'InService'
-            instance = AWS::EC2::Instance.new instance_health[:instance].id
-            puts instance.dns_name
+        as_sg = AWS::AutoScaling.new
+        sg = as_sg.groups
+        sg.each do |x|
+            puts x.name
+            x.auto_scaling_instances.each do |instance|
+                puts ec2.instances[instance.instance_id].dns_name
+            end
         end
     end
-    puts '---'
-    puts
 
+    def get_by_elb
+        elb = AWS::ELB.new
+
+        elb.load_balancers.each do |e|
+
+            ic = AWS::ELB::InstanceCollection.new e
+
+            puts "---- #{ic.load_balancer.name}"
+            ic.health.each do |instance_health|
+                if instance_health[:state] == 'InService'
+                    instance = AWS::EC2::Instance.new instance_health[:instance].id
+                    puts "ssh ubuntu@#{instance.dns_name}"
+                end
+            end
+
+        end
+    end
 end
 
-#ec2.instances.each do |instance|
-#	puts " #{instance.dns_name} #{instance.status} "
-#end
+awsinfo = AwsInfo.new
+awsinfo.parse_config_files 'config.yml', 'secrets.yml'
+awsinfo.get_by_elb
+awsinfo.get_by_autoscaling_group
